53b4b8ae151919c0ffac6ff32c7872e94ee6914e
Nigel Stephen D'Souza <nigelsdtech@gmail.com>
2018-08-28 00:13:07 +0100

    Generic log and report names

--- config/default.js
+++ config/default.js
@@ -15,16 +15,21 @@ module.exports = {
 
 
   log: {
-    appName: defer(function (cfg) { return cfg.appName } ),
     level:   "INFO",
     log4jsConfigs: {
       appenders: [
         {
           type:       "file",
-          filename:   defer(function (cfg) { return cfg.log.logDir.concat("/" , cfg.appName, "-", process.env.NODE_ENV, ".log" ) }),
-          category:   defer(function (cfg) { return cfg.log.appName }),
+          filename:   defer(function (cfg) {
+            var name = cfg.log.logDir + "/" + cfg.appName
+            if (process.env.NODE_APP_INSTANCE) { name += '-' + process.env.NODE_APP_INSTANCE }
+            if (process.env.NODE_ENV)          { name += '-' + process.env.NODE_ENV }
+            name += ".log"
+            return name
+          }),
+          category:   defer(function (cfg) { return cfg.appName }),
           reloadSecs: 60,
-          maxLogSize: 1024000
+          maxLogSize: 1024000000
         },
         {
           type: "console"
@@ -38,7 +43,12 @@ module.exports = {
   maxResults: 10,
 
   reporter: {
-    appName             : defer( function (cfg) { return cfg.appName } ),
+    appName             : defer( function (cfg) {
+      var name = cfg.appName
+      if (process.env.NODE_APP_INSTANCE) { name += '-' + process.env.NODE_APP_INSTANCE }
+      if (process.env.NODE_ENV)          { name += '-' + process.env.NODE_ENV }
+      return name
+    }),
     appSpecificPassword : "OVERRIDE_ME",
     emailsFrom          : "OVERRIDE_ME",
     name                : "Reporter (Personal)",
